b883668c89a54283054324993575420965c1370f
Rishab2000 <rishabsachidanand@gmail.com>
2020-09-06 21:54:09 +0530

    Challenge 5

---
+++ Challenge_5/index.js
@@ -0,0 +1,26 @@
+var i = 1;
+
+function cardCycle() {
+  if (i < 5) {
+    $(".card-" + i).toggleClass("fade-out");
+    console.log(".card-" + i);
+    $(".card-" + (i + 1)).toggleClass("fade-in");
+    i++;
+    console.log(i);
+  }
+  if (i == 4) {
+    i = 1;
+    console.log(i);
+    $(".card-1").toggleClass("fade-out");
+    $(".card-2").toggleClass("fade-out");
+    $(".card-2").toggleClass("fade-in");
+    $(".card-3").toggleClass("fade-in");
+    setTimeout(function () {
+      $(".card-3").toggleClass("fade-out");
+    }, 1000);
+  }
+}
+
+$(".button").on("click", function () {
+  cardCycle();
+});
