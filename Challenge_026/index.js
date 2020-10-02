5aed7ebdd3e07275aa599deb17e23eceec87ef91
Rishab2000 <rishabsachidanand@gmail.com>
2020-10-02 22:26:32 +0530

    Challenge 26

---
+++ Challenge_026/index.js
@@ -0,0 +1,24 @@
+$(".button_face").on("click", function () {
+  $(".button").toggleClass("button_flip");
+});
+
+$(".button_face").hover(
+  function (event) {
+    var parentOffest = $(this).offset();
+    var pX = event.pageX - parentOffest.left;
+    var pY = event.pageY - parentOffest.top;
+
+    $(".inner").css({ left: pX, top: pY });
+    $(".inner").addClass("explode");
+    $(".inner").removeClass("displode");
+  },
+  function (event) {
+    var parentOffest = $(this).offset();
+    var pX = event.pageX - parentOffest.left;
+    var pY = event.pageY - parentOffest.top;
+
+    $(".inner").css({ left: pX, top: pY });
+    $(".inner").addClass("displode");
+    $(".inner").removeClass("explode");
+  }
+);
